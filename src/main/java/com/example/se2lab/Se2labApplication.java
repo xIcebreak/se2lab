60ed3e0c32f9087008b8594eb2e5a6650f0f07e6
Max Eisbrich <maximilian.eisbrich@gmail.com>
2023-05-10 22:35:53 +0200

    first commit

--- src/main/java/com/example/se2lab/Se2labApplication.java
+++ src/main/java/com/example/se2lab/Se2labApplication.java
@@ -3,11 +3,27 @@ package com.example.se2lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Se2labApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Se2labApplication.class, args);
+        connectDatabase();
+    }
+
+    private static void connectDatabase() {
+        final String url = "jdbc:mysql://localhost:3306/demoinventorydb";
+        final String username = "root";
+        final String password = "mysqldbpw123$";
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
     }
 
 }
--- src/main/java/com/example/se2lab/benutzer/dataaccess/api/repo/BenutzerRepository.java
+++ src/main/java/com/example/se2lab/benutzer/dataaccess/api/repo/BenutzerRepository.java
@@ -1,6 +1,7 @@
 package com.example.se2lab.benutzer.dataaccess.api.repo;
 
 import com.example.se2lab.benutzer.dataaccess.api.entity.Benutzer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BenutzerRepository extends JpaRepository<Benutzer,Long>{
+public interface BenutzerRepository extends JpaRepository<Benutzer,Long> {
 }
