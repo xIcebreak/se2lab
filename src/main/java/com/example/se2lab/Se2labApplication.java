42e56b8893a32f69cb88ab9a73ef530b45b27179
Max Eisbrich <maximilian.eisbrich@gmail.com>
2023-05-10 20:39:04 +0200

    first commit

---
+++ src/main/java/com/example/se2lab/Se2labApplication.java
@@ -0,0 +1,13 @@
+package com.example.se2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2labApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se2labApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/se2lab/Se2labApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se2lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2labApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
